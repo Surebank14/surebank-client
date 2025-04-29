addedea6d26db9edce54b2723856244db413508e
joseph abakpa <elachi4c@yahoo.com>
2025-04-29 17:30:12 +0100

    first commit

---
+++ src/Components/Table/TransactionTableHead.jsx
@@ -0,0 +1,15 @@
+const Tablehead = () => {
+    return (
+      <thead className="text-xs">
+        <tr className="bg-gray-100">
+          <th className="text-left"><p className="ml-2">Date</p></th>
+          <th className="text-left"><p className="ml-2">Amount</p></th>
+          <th className="text-left"><p className="ml-2">Naration</p></th>
+          <th className="text-left"><p>Balance</p></th>
+        </tr>
+      </thead>
+    );
+  };
+  
+  export default Tablehead;
+  
