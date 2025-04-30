cf5df371e9f9deec599bf21021af586e533f7699
joseph abakpa <elachi4c@yahoo.com>
2025-04-30 14:13:56 +0100

    whatsapp3

--- src/Components/CustomerAccountDashboard.jsx
+++ src/Components/CustomerAccountDashboard.jsx
@@ -70,7 +70,7 @@ const CustomerAccountDashboard = () => {
   </button>
 
   <a
-    href="https://wa.me/08031313024"
+    href="https://wa.me/08026211164"
     target="_blank"
     rel="noreferrer"
     className="text-xl text-green-600 hover:text-blue-700"
